19ba3dfd3e19bc72dcb572b8d73e8a5181e15fd2
fn-static <onlyfanskasia111@gmail.com>
2024-08-28 23:21:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const bodyParser = require('body-parser');
 const stripe = require('stripe')('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'); // Zamień na swój klucz tajny Stripe
 
 const app = express();
-const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/create-payment-intent', async (req, res) => {
